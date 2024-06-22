53562dc4709ea7352ce454a259ab07d9fce2d58b
MOHAMAD-ZUBI <mozubi128@hotmail.com>
2024-06-23 00:31:53 +0300

    removed footer from layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,7 +33,6 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <NavBar />
         {children}
-        <Footer />
       </body>
     </html>
   );
