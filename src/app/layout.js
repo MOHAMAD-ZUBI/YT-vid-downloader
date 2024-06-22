2aeec8fa4f69d770ea9307ce20b14fed5ab340d0
MOHAMAD-ZUBI <mozubi128@hotmail.com>
2024-06-23 00:30:21 +0300

    feat: Add Google Analytics tracking to application
    This commit adds Google Analytics tracking to the application by including the necessary script and configuration in the layout.js file. The tracking code is asynchronously loaded from the Google Tag Manager and configured to track page views. This addition allows for better understanding of user behavior and helps improve the application based on analytics data.

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/main/NavBar";
 import Footer from "@/components/main/Footer";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,9 +14,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-W177CFHW1H"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-W177CFHW1H');
+            `,
+          }}
+        />
+      </Head>
       <body className={inter.className}>
         <NavBar />
         {children}
+        <Footer />
       </body>
     </html>
   );
